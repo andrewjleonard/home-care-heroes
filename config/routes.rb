c7516c82f772b0a5b7504e3edc225336fe134705
Andrew Leonard <professional@aleonard.co.uk>
2018-01-27 16:12:14 +1100

    Added User controller so list of users is possible. This is the first stage of the messaging ability

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
   root 'pages#home'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
